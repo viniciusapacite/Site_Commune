2b74a6ae3d0d511c965592e2b461b998cbcc64a1
vinicius apacite <viniapacite14@gmail.com>
2022-12-29 20:28:58 -0300

    Subindo Arquivos da landing page

---
+++ toggleMenu.js
@@ -0,0 +1,31 @@
+
+
+
+
+ function menuAbrir(){
+     var menu = document.querySelectorAll('.toggle-menu');
+     for(var i = 0; i < menu.length; i++){
+         menu[i].addEventListener('click', ()=>{
+             var nav = document.querySelectorAll('nav.toggle-bar');
+             for(var i = 0; i < nav.length; i++){
+                 if(nav[i].style.display == 'flex'){
+                     nav[i].style.display = 'none';
+                     nav[i].style.transition = '2s all';
+                 }
+                 else{
+                     nav[i].style.display ='flex';
+                     nav[i].style.transition = '1s all';
+                 }
+             }
+         })
+     }
+ }
+
+ menuAbrir();
+
+   
+
+
+        
+       
+  
